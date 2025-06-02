9312749bd7a6184fcb3ea63cb5c93dd37fc4cd98
26nafis <spalanzaninafis@gmail.com>
2025-06-02 11:11:13 +0700

    membuat class joko

---
+++ abstraksi/polimorpi/polimorpi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+    public:
+virtual void pesan()= 0;
+// virtual void pesan(){
+// cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+    //}
+};
+
+#include <iostream>
+using namespace std;
+
+class joko : public seseorang {
+public:
+    void pesan() {
+        cout << "pesan dari joko " << endl;
+    }
+};
+
+
