8d921794e3f17e97e1eccfb8626431c7094fbf7c
26nafis <spalanzaninafis@gmail.com>
2025-06-02 10:25:23 +0700

    membuat class orang

--- abstraksi/enkapsulasi/enkapsulasi.cpp
+++ abstraksi/enkapsulasi/enkapsulasi.cpp
@@ -11,3 +11,17 @@ using namespace std;
                 return saklarNo[i];
         }
  };
+
+ int main (){
+    remoteLampu lampuRumah;
+
+    lampuRumah.setsaklarNo(0, "lampu teras rumah");
+    lampuRumah.setsaklarNo(1, "lampu ruang tamu");
+    lampuRumah.setsaklarNo(2, "lampu kamar tidur");
+    lampuRumah.setsaklarNo(3, "lampu teras rumah");
+
+    cout << lampuRumah.getsaklarNO(0) << endl;
+    cout << lampuRumah.getsaklarNO(1) << endl;
+    cout << lampuRumah.getsaklarNO(1) << endl;
+    cout << lampuRumah.getsaklarNO(3) << endl;
+ }
